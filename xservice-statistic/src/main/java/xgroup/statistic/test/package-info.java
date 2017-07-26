64099fa92753399b55f63abb572e10ee394e0105
lufei <lufei@01zhuanche.com>
2017-07-26 09:47:01 +0800

    modify package struct

--- xservice-statistic/src/main/java/xgroup/statistic/test/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lufei
- * Date : 2017/7/25 14:27
- * Description :
- */
-package xgroup.statistic.test;
--- xservice-statistic/src/main/java/xgroup/statistic/tp/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lufei
- * Date : 2017/7/25 15:13
- * Description :
- */
-package xgroup.statistic.tp;
