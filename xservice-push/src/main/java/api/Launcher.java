6ddeb150809c0820a8e9b2234b5ab0e6262e975e
yanglongfei <yanglongfei@01zhuanche.com>
2017-07-27 17:41:19 +0800

    code commit

--- xservice-push/src/main/java/api/Launcher.java
+++ xservice-push/src/main/java/api/Launcher.java
@@ -1,6 +1,7 @@
 package api;
 
 import channel.AmqpConsumerVerticle;
+import channel.HttpConsumerVerticle;
 import channel.MiPushVerticle;
 import channel.SocketVerticle;
 import io.vertx.core.Vertx;
@@ -9,8 +10,10 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		Vertx vertx = Vertx.vertx();
-
-		vertx.deployVerticle(AmqpConsumerVerticle.class.getName());
+		
+		
+		vertx.deployVerticle(HttpConsumerVerticle.class.getName());
+		//vertx.deployVerticle(AmqpConsumerVerticle.class.getName());
 		vertx.deployVerticle(MiPushVerticle.class.getName());
 		vertx.deployVerticle(SocketVerticle.class.getName());
 	}
