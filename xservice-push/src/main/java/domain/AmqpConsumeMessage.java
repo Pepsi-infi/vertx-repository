a3abe84ec5d9ce48dea73391899636747cf1cc5f
yanglongfei <yanglongfei@01zhuanche.com>
2017-07-27 11:16:56 +0800

    消息体实体类属性变更

--- xservice-push/src/main/java/domain/AmqpConsumeMessage.java
+++ xservice-push/src/main/java/domain/AmqpConsumeMessage.java
@@ -15,7 +15,7 @@ public class AmqpConsumeMessage extends BaseModel implements Serializable{
 	 */
 	private static final long serialVersionUID = -593071374368333868L;
 	private String amqpMsgId;//消费后生成的msgId
-	private MsgBody msgBody;//消息体内容  json格式的字符串
+	private String msgBody;//消息体内容  json格式的字符串
 	private int status;//消息状态  0消费失败  1消费成功  2未知
 	private String channel;//推送渠道  socket  xiaomi  gcm 
 	
@@ -26,10 +26,11 @@ public class AmqpConsumeMessage extends BaseModel implements Serializable{
 		this.amqpMsgId = amqpMsgId;
 	}
 	
-	public MsgBody getMsgBody() {
+	
+	public String getMsgBody() {
 		return msgBody;
 	}
-	public void setMsgBody(MsgBody msgBody) {
+	public void setMsgBody(String msgBody) {
 		this.msgBody = msgBody;
 	}
 	public int getStatus() {
