58b7013c22150b40927b40cf28e20d86d628f7fd
wanglonghu <wanglonghu@01zhuanche.com>
2017-07-29 17:14:24 +0800

    BUILD_ID

--- xservice-push/bin/start-dev.sh
+++ xservice-push/bin/start-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
 root_path=$(cd "$(dirname "${0}")"; pwd)
-nohup java \
+BUILD_ID=
+java \
 -server \
 -XX:+PrintGCApplicationStoppedTime \
 -XX:+PrintGCTimeStamps \
@@ -12,9 +13,9 @@ nohup java \
 -XX:MaxTenuringThreshold=2 -XX:+ExplicitGCInvokesConcurrent \
 -XX:-UseCounterDecay \
 -Djava.net.preferIPv4Stack=true \
--Xloggc:/u01/projectCAR/service/xservice-push/gc.log \
--Dlog.path=/u01/projectCAR/service/xservice-push \
+-Xloggc:${root_path}/gc.log \
+-Dlog.path=${root_path}/ \
 -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
 -Dlog4j.configurationFile=log4j2.xml \
 -Dvertx.zookeeper.config=zookeeper-dev.json \
--jar ${root_path}/xservice-push-fat.jar > /dev/null 2>&1 &
+-jar ${root_path}/xservice-push-fat.jar
