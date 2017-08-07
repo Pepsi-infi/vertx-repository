349a97d2c1a65088384325b403e3bc058c27691e
lufei <lufei@01zhuanche.com>
2017-08-07 19:10:55 +0800

    设备上报接口修改

--- xservice-common/src/main/java/iservice/dto/DeviceDto.java
+++ xservice-common/src/main/java/iservice/dto/DeviceDto.java
@@ -17,14 +17,10 @@ public class DeviceDto {
     private String phone;
     //设备型号
     private String deviceType;
-    //小米设备token
-    private String miToken;
-    //gcmToken
-    private String gcmToken;
-    //apple推送token
-    private String apnsToken;
-    //
-    private String imei;
+    //push 渠道
+    private Integer channel;
+    //对应渠道token
+    private String deviceToken;
     //系统类型
     private Integer osType;
     //系统类型
@@ -60,22 +56,6 @@ public class DeviceDto {
 
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public String getMiToken() {
-        return miToken;
-    }
-
-    public void setMiToken(String miToken) {
-        this.miToken = miToken;
-    }
-
-    public String getImei() {
-        return imei;
-    }
-
-    public void setImei(String imei) {
-        this.imei = imei;
     }
 
     public Integer getOsType() {
@@ -134,20 +114,20 @@ public class DeviceDto {
         this.isAcceptPush = isAcceptPush;
     }
 
-    public String getGcmToken() {
-        return gcmToken;
+    public Integer getChannel() {
+        return channel;
     }
 
-    public void setGcmToken(String gcmToken) {
-        this.gcmToken = gcmToken;
+    public void setChannel(Integer channel) {
+        this.channel = channel;
     }
 
-    public String getApnsToken() {
-        return apnsToken;
+    public String getDeviceToken() {
+        return deviceToken;
     }
 
-    public void setApnsToken(String apnsToken) {
-        this.apnsToken = apnsToken;
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
     }
 
     public DeviceDto() {
@@ -165,47 +145,6 @@ public class DeviceDto {
         return json;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        DeviceDto deviceDto = (DeviceDto) o;
-
-        if (uid != null ? !uid.equals(deviceDto.uid) : deviceDto.uid != null) return false;
-        if (phone != null ? !phone.equals(deviceDto.phone) : deviceDto.phone != null) return false;
-        if (deviceType != null ? !deviceType.equals(deviceDto.deviceType) : deviceDto.deviceType != null) return false;
-        if (miToken != null ? !miToken.equals(deviceDto.miToken) : deviceDto.miToken != null)
-            return false;
-        if (gcmToken != null ? !gcmToken.equals(deviceDto.gcmToken) : deviceDto.gcmToken != null) return false;
-        if (apnsToken != null ? !apnsToken.equals(deviceDto.apnsToken) : deviceDto.apnsToken != null) return false;
-        if (imei != null ? !imei.equals(deviceDto.imei) : deviceDto.imei != null) return false;
-        if (osType != null ? !osType.equals(deviceDto.osType) : deviceDto.osType != null) return false;
-        if (osVersion != null ? !osVersion.equals(deviceDto.osVersion) : deviceDto.osVersion != null) return false;
-        if (appCode != null ? !appCode.equals(deviceDto.appCode) : deviceDto.appCode != null) return false;
-        if (appVersion != null ? !appVersion.equals(deviceDto.appVersion) : deviceDto.appVersion != null) return false;
-        if (!antFingerprint.equals(deviceDto.antFingerprint)) return false;
-        return isAcceptPush.equals(deviceDto.isAcceptPush);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = uid != null ? uid.hashCode() : 0;
-        result = 31 * result + (phone != null ? phone.hashCode() : 0);
-        result = 31 * result + (deviceType != null ? deviceType.hashCode() : 0);
-        result = 31 * result + (miToken != null ? miToken.hashCode() : 0);
-        result = 31 * result + (gcmToken != null ? gcmToken.hashCode() : 0);
-        result = 31 * result + (apnsToken != null ? apnsToken.hashCode() : 0);
-        result = 31 * result + (imei != null ? imei.hashCode() : 0);
-        result = 31 * result + (osType != null ? osType.hashCode() : 0);
-        result = 31 * result + (osVersion != null ? osVersion.hashCode() : 0);
-        result = 31 * result + (appCode != null ? appCode.hashCode() : 0);
-        result = 31 * result + (appVersion != null ? appVersion.hashCode() : 0);
-        result = 31 * result + antFingerprint.hashCode();
-        result = 31 * result + isAcceptPush.hashCode();
-        return result;
-    }
 
     @Override
     public String toString() {
@@ -214,10 +153,8 @@ public class DeviceDto {
         sb.append(", uid=").append(uid);
         sb.append(", phone='").append(phone).append('\'');
         sb.append(", deviceType='").append(deviceType).append('\'');
-        sb.append(", miToken='").append(miToken).append('\'');
-        sb.append(", gcmToken='").append(gcmToken).append('\'');
-        sb.append(", apnsToken='").append(apnsToken).append('\'');
-        sb.append(", imei='").append(imei).append('\'');
+        sb.append(", channel=").append(channel);
+        sb.append(", deviceToken='").append(deviceToken).append('\'');
         sb.append(", osType=").append(osType);
         sb.append(", osVersion='").append(osVersion).append('\'');
         sb.append(", appCode=").append(appCode);
