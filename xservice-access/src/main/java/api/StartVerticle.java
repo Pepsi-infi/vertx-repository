3c8cdfe8b519900ac8bc42b7a8da826dd5268e62
wanglonghu <wanglonghu@01zhuanche.com>
2017-08-08 14:04:45 +0800

    增加http server&&手动修改ip

--- xservice-access/src/main/java/api/StartVerticle.java
+++ xservice-access/src/main/java/api/StartVerticle.java
@@ -3,6 +3,7 @@ package api;
 import cluster.impl.ConsistentHashingVerticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import server.HttpServerVerticle;
 import server.TCPServerVerticle;
 
 public class StartVerticle extends AbstractVerticle {
@@ -10,6 +11,7 @@ public class StartVerticle extends AbstractVerticle {
 	public void start() throws Exception {
 		vertx.deployVerticle(TCPServerVerticle.class.getName(), readBossOpts().setConfig(config()));
 		vertx.deployVerticle(ConsistentHashingVerticle.class.getName(), readBossOpts().setConfig(config()));
+		vertx.deployVerticle(HttpServerVerticle.class.getName(), readBossOpts().setConfig(config()));
 	};
 
 	public static DeploymentOptions readBossOpts() {
