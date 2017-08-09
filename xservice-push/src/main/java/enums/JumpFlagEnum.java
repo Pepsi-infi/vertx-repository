bf266163f41805ebb111bf9032ed5dec6c275d9e
yanglongfei <yanglongfei@01zhuanche.com>
2017-08-09 20:57:21 +0800

    推送跳转页映射关系更改

--- xservice-push/src/main/java/enums/JumpFlagEnum.java
+++ xservice-push/src/main/java/enums/JumpFlagEnum.java
@@ -4,11 +4,13 @@ public enum JumpFlagEnum {
 	
 	MESSAGE_CENTER_PAGE(0,"sqyc://01zhuanche.com/messageCenter","消息中心页"),
 	MEMBER_LEVEL_PAGE(1,"sqyc://01zhuanche.com/memberLevel","用户等级页"),
-	MEMBER_ACCOUNT_PAGE(2,"sqyc://01zhuanche.com/account","个人账户页"),
-	CREDIT_CARD_PAGE(3,"sqyc://01zhuanche.com/creditCard","信用卡页"),
-	MEMBER_JOURNEY_PAGE(4,"sqyc://01zhuanche.com/mytrip","用户行程页"),
+	INVITE_FRIENDS_PAGE(2,"sqyc://01zhuanche.com/inviteFriends","邀请好友页"),
+	MEMBER_JOURNEY_PAGE(3,"sqyc://01zhuanche.com/mytrip","用户行程页"),
+	MEMBER_ACCOUNT_PAGE(4,"sqyc://01zhuanche.com/account","个人账户页"),
 	MEMBER_COUPON_PAGE(5,"sqyc://01zhuanche.com/coupon","用户优惠券页"),
-	INVITE_FRIENDS_PAGE(6,"sqyc://01zhuanche.com/inviteFriends","邀请好友页");
+	CREDIT_CARD_PAGE(6,"sqyc://01zhuanche.com/creditCard","信用卡页");
+	
+	
 	
 	private int code;
 	private String msg;
