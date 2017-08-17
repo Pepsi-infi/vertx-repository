d6dfa341795d0c597cf9bc6e998d8417c7d3a19b
wanglonghu <wanglonghu@01zhuanche.com>
2017-08-17 14:38:18 +0800

    集成Redis cluster. Complete.

---
+++ xservice-common/src/main/java/redis/HostAndPort.java
@@ -0,0 +1,34 @@
+package redis;
+
+public class HostAndPort {
+    private String host;
+    private Integer port;
+
+    public HostAndPort() { }
+
+    public HostAndPort(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return "{host='" + host + "', port=" + port + '}';
+}
+}
