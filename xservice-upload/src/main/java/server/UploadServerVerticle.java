d2ee54590bc2281b8e0b696ff463a78c6c8b5444
wanglonghu <wanglonghu@01zhuanche.com>
2017-08-29 20:52:31 +0800

    文件上传、下载服务器

--- xservice-upload/src/main/java/server/UploadServerVerticle.java
+++ xservice-upload/src/main/java/server/UploadServerVerticle.java
@@ -9,8 +9,11 @@ import api.RestConstant;
 import constant.UploadConstant;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.file.FileProps;
 import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -29,47 +32,72 @@ public class UploadServerVerticle extends AbstractVerticle {
 		HttpServer httpServer = vertx.createHttpServer();
 
 		httpServer.requestHandler(request -> {
-			switch (request.uri()) {
-			case RestConstant.Uri.UPLOAD_FILE_PATH:
-				String uuid = UUID.randomUUID().toString();
-				request.setExpectMultipart(true);
-				LocalDate date = LocalDate.now();
-				String uploadPath = UploadConstant.UPLOAD_FILE_PATH_PREFIX + date + "/";
-
-				if ((StringUtils.isEmpty(lastCreated) || !lastCreated.equalsIgnoreCase(date.toString()))
-						&& !fs.existsBlocking(uploadPath)) {
-					fs.mkdirBlocking(uploadPath);
-					lastCreated = date.toString();
-					logger.info("Create dir " + uploadPath + " lastCreated " + lastCreated);
-				}
-				request.uploadHandler(upload -> {
-					upload.streamToFileSystem(uploadPath + uuid).endHandler(a -> {
-
-						System.out.println("end formAttributes" + request.formAttributes());
-
-						JsonObject response = new JsonObject();
-						response.put("code", 0);
-						response.put("time", System.currentTimeMillis());
-						request.response().end(response.encode());
+			if (request.method() == HttpMethod.GET) {
+				String file = request.getParam("file");
+				switch (request.path()) {
+				case RestConstant.Uri.DOWNLOAD_FILE_PATH:
+					String sysFile = UploadConstant.UPLOAD_FILE_PATH_PREFIX + file;
+					fs.exists(sysFile, res -> {
+						if (!res.result()) {
+							sendNotFound(request);
+						} else {
+							fs.props(sysFile, prop -> {
+								FileProps props = prop.result();
+								request.response().putHeader("Content-Length", Long.toString(props.size()));
+								request.response().sendFile(sysFile);
+							});
+						}
 					});
-				});
 
-				break;
-			case "/index":
-				request.response().sendFile("webroot/index.html");
+					break;
+				default:
+					sendNotFound(request);
+					break;
+				}
+			} else {
+				switch (request.uri()) {
+				case RestConstant.Uri.UPLOAD_FILE_PATH:
+					String uuid = UUID.randomUUID().toString();
+					request.setExpectMultipart(true);
+					LocalDate date = LocalDate.now();
+					String uploadPath = UploadConstant.UPLOAD_FILE_PATH_PREFIX + date + "/";
 
-				break;
-			default:
-				JsonObject response = new JsonObject();
-				response.put("code", 1);
-				response.put("msg", "Cannot not locate resource " + request.uri());
-				response.put("time", System.currentTimeMillis());
-				request.response().setStatusCode(404).putHeader("content-type", "application/json")
-						.end(response.encode());
+					if ((StringUtils.isEmpty(lastCreated) || !lastCreated.equalsIgnoreCase(date.toString()))
+							&& !fs.existsBlocking(uploadPath)) {
+						fs.mkdirBlocking(uploadPath);
+						lastCreated = date.toString();
+						logger.info("Create dir " + uploadPath + " lastCreated " + lastCreated);
+					}
+					request.uploadHandler(upload -> {
+						upload.streamToFileSystem(uploadPath + uuid).endHandler(a -> {
 
-				break;
+							System.out.println("end formAttributes" + request.formAttributes());
+
+							JsonObject response = new JsonObject();
+							response.put("code", 0);
+							response.put("time", System.currentTimeMillis());
+							request.response().end(response.encode());
+						});
+					});
+
+					break;
+				case "/index":
+					request.response().sendFile("webroot/index.html");
+
+					break;
+				default:
+					sendNotFound(request);
+					break;
+				}
 			}
-
 		}).listen(RestConstant.Server.PORT);
+	}
+
+	private void sendNotFound(HttpServerRequest request) {
+		JsonObject response = new JsonObject();
+		response.put("code", 1000);
+		response.put("msg", "Cannot not locate resource " + request.uri());
+		response.put("time", System.currentTimeMillis());
+		request.response().setStatusCode(404).putHeader("content-type", "application/json").end(response.encode());
 	}
 }
