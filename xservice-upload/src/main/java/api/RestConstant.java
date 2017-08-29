06f8c6428df1fdb3c20c9694c633de596e4673eb
wanglonghu <wanglonghu@01zhuanche.com>
2017-08-29 18:27:17 +0800

    文件服务器完善

---
+++ xservice-upload/src/main/java/api/RestConstant.java
@@ -0,0 +1,12 @@
+package api;
+
+public class RestConstant {
+
+	public interface Server {
+		static final int PORT = 9090;
+	}
+
+	public interface Uri {
+		static final String UPLOAD_FILE_PATH = "/mc-file/upload.json";
+	}
+}
--- xservice-upload/src/main/java/server/UploadServerVerticle.java
+++ xservice-upload/src/main/java/server/UploadServerVerticle.java
@@ -1,42 +1,75 @@
 package server;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
+import com.baidu.bjf.remoting.protobuf.utils.StringUtils;
+
+import api.RestConstant;
+import constant.UploadConstant;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class UploadServerVerticle extends AbstractVerticle {
 
-	@Override
-	public void start() throws Exception {
+	private static final Logger logger = LoggerFactory.getLogger(UploadServerVerticle.class);
 
-		Router router = Router.router(vertx);
+	private FileSystem fs;
+
+	private String lastCreated;
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		fs = vertx.fileSystem();
 		HttpServer httpServer = vertx.createHttpServer();
 
-		router.route("/").handler(StaticHandler.create("webroot").setIndexPage("index.html"));
-
 		httpServer.requestHandler(request -> {
-			request.setExpectMultipart(true);
-			request.uploadHandler(upload -> {
-				upload.streamToFileSystem("/Users/wanglonghu/temp/" + upload.filename() + System.currentTimeMillis())
-						.endHandler(a -> {
+			switch (request.uri()) {
+			case RestConstant.Uri.UPLOAD_FILE_PATH:
+				String uuid = UUID.randomUUID().toString();
+				request.setExpectMultipart(true);
+				LocalDate date = LocalDate.now();
+				String uploadPath = UploadConstant.UPLOAD_FILE_PATH_PREFIX + date + "/";
 
-							System.out.println("end absoluteURI" + request.absoluteURI());
-							System.out.println("end formAttributes" + request.formAttributes());
-							System.out.println("end headers" + request.headers());
-							System.out.println("end params" + request.params());
-							System.out.println("end path" + request.path());
-							System.out.println("end uri" + request.uri());
-							
-							request.response().end("3Q");
-						});
-			});
-		}).listen(9090);
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(LocalDate.now());
+				if ((StringUtils.isEmpty(lastCreated) || !lastCreated.equalsIgnoreCase(date.toString()))
+						&& !fs.existsBlocking(uploadPath)) {
+					fs.mkdirBlocking(uploadPath);
+					lastCreated = date.toString();
+					logger.info("Create dir " + uploadPath + " lastCreated " + lastCreated);
+				}
+				request.uploadHandler(upload -> {
+					upload.streamToFileSystem(uploadPath + uuid).endHandler(a -> {
+
+						System.out.println("end formAttributes" + request.formAttributes());
+
+						JsonObject response = new JsonObject();
+						response.put("code", 0);
+						response.put("time", System.currentTimeMillis());
+						request.response().end(response.encode());
+					});
+				});
+
+				break;
+			case "/index":
+				request.response().sendFile("webroot/index.html");
+
+				break;
+			default:
+				JsonObject response = new JsonObject();
+				response.put("code", 1);
+				response.put("msg", "Cannot not locate resource " + request.uri());
+				response.put("time", System.currentTimeMillis());
+				request.response().setStatusCode(404).putHeader("content-type", "application/json")
+						.end(response.encode());
+
+				break;
+			}
+
+		}).listen(RestConstant.Server.PORT);
 	}
 }
