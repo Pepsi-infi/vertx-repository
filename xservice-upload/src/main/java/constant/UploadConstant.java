5ac0d2cfaed85a864b16558813303856a9cd46ee
wanglonghu <wanglonghu@01zhuanche.com>
2017-08-29 18:27:17 +0800

    文件服务器完善

--- xservice-upload/src/main/java/constant/UploadConstant.java
+++ xservice-upload/src/main/java/constant/UploadConstant.java
@@ -2,6 +2,6 @@ package constant;
 
 public class UploadConstant {
 
-	public static final String UPLOAD_FILE_PATH = "/u01/mc";
+	public static final String UPLOAD_FILE_PATH_PREFIX = "/Users/wanglonghu/temp/";
 
 }
