e4294d6b7fa77e6b9a7b7a653a9430804136e132
yanglongfei <yanglongfei@01zhuanche.com>
2017-09-01 16:23:24 +0800

    消息推送改造 -  区分出 大后台广告消息推送     乘客消息推送

--- xservice-push/src/main/java/api/StartVerticle.java
+++ xservice-push/src/main/java/api/StartVerticle.java
@@ -1,6 +1,7 @@
 package api;
 
 import channel.ApplePushVerticle;
+import channel.HttpServerVerticle;
 import channel.MessagePushContainer;
 import channel.MessagePushNonAdver;
 import channel.MiPushVerticle;
@@ -10,41 +11,48 @@ import io.vertx.core.Future;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.rxjava.core.AbstractVerticle;
+import service.AdMessagePushService;
+import service.impl.AdMessagePushServiceImpl;
 import service.impl.MsgRecordServiceImpl;
+import service.impl.NonAdMessagePushServiceImpl;
 import service.impl.RedisServiceImpl;
 import xservice.HttpClientVerticle;
 
-public class StartVerticle extends AbstractVerticle{
+public class StartVerticle extends AbstractVerticle {
 
-    private static final Logger logger = LoggerFactory.getLogger(StartVerticle.class);
-    @Override
-    public void start() throws Exception {
-        super.start();
-        // 提供EventBus服务
+	private static final Logger logger = LoggerFactory.getLogger(StartVerticle.class);
 
-        this.deployVerticle(MsgRecordServiceImpl.class.getName());
-        this.deployVerticle(RedisServiceImpl.class.getName());
+	@Override
+	public void start() throws Exception {
+		super.start();
+		// 提供EventBus服务
 
-        this.deployVerticle(MiPushVerticle.class.getName());
+		this.deployVerticle(MsgRecordServiceImpl.class.getName());
+		this.deployVerticle(RedisServiceImpl.class.getName());
+
+		this.deployVerticle(MiPushVerticle.class.getName());
 		this.deployVerticle(SocketVerticle.class.getName());
-        this.deployVerticle(ApplePushVerticle.class.getName());
+		this.deployVerticle(ApplePushVerticle.class.getName());
 
-        this.deployVerticle(MessagePushContainer.class.getName());
-        this.deployVerticle(HttpClientVerticle.class.getName());
-        this.deployVerticle(MessagePushNonAdver.class.getName());
-        // 提供其他非EventBus服务
-    }
+		// this.deployVerticle(MessagePushContainer.class.getName());
+		this.deployVerticle(HttpClientVerticle.class.getName());
+		// this.deployVerticle(MessagePushNonAdver.class.getName());
+		this.deployVerticle(HttpServerVerticle.class.getName());
+		this.deployVerticle(AdMessagePushServiceImpl.class.getName());
+		this.deployVerticle(NonAdMessagePushServiceImpl.class.getName());
+		// 提供其他非EventBus服务
+	}
 
-    public void deployVerticle(String verticleName) {
-        Future<String> future = Future.future();
-        future.setHandler(ar -> logger.info(ar.succeeded() ? "success:" + ar.result() : "failed:" + ar.cause()));
-        vertx.deployVerticle(verticleName, readBossOpts().setConfig(config()), future.completer());
-    }
+	public void deployVerticle(String verticleName) {
+		Future<String> future = Future.future();
+		future.setHandler(ar -> logger.info(ar.succeeded() ? "success:" + ar.result() : "failed:" + ar.cause()));
+		vertx.deployVerticle(verticleName, readBossOpts().setConfig(config()), future.completer());
+	}
 
-    public static DeploymentOptions readBossOpts() {
-        DeploymentOptions options = new DeploymentOptions();
-        options.setInstances(Runtime.getRuntime().availableProcessors());
-        return options;
-    }
+	public static DeploymentOptions readBossOpts() {
+		DeploymentOptions options = new DeploymentOptions();
+		options.setInstances(Runtime.getRuntime().availableProcessors());
+		return options;
+	}
 
 }
