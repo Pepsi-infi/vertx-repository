7c41da83063974799558c1a9ce0a06eaa07c0efe
wanglonghu <wanglonghu@01zhuanche.com>
2017-09-25 21:31:03 +0800

    final start

--- xservice-im/bin/start-dev.sh
+++ xservice-im/bin/start-dev.sh
@@ -26,26 +26,6 @@ fi
 -Dlog4j.configurationFile=log4j2.xml \
 -Dconfig=dev \
 -Dvertx.zookeeper.config=zookeeper-dev.json \
--jar ${root_path}/xservice-im-fat.jar >> ${root_path}/nohup.out1 &
-
-nohup /usr/local/jdk1.8/bin/java \
--server \
--XX:+PrintGCApplicationStoppedTime \
--XX:+PrintGCTimeStamps \
--XX:+PrintGCDetails \
--Xms2g -Xmx2g -Xmn1380m -Xss256K -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m \
--XX:MaxDirectMemorySize=2560m \
--XX:AutoBoxCacheMax=20000 -XX:+AlwaysPreTouch \
--XX:+UseParallelOldGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly \
--XX:MaxTenuringThreshold=2 -XX:+ExplicitGCInvokesConcurrent \
--XX:-UseCounterDecay \
--Djava.net.preferIPv4Stack=true \
--Xloggc:${root_path}/gc.log \
--Dlog.path=${root_path}/log \
--Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
--Dlog4j.configurationFile=log4j2.xml \
--Dconfig=dev \
--Dvertx.zookeeper.config=zookeeper-dev.json \
 -jar ${root_path}/xservice-im-fat.jar >> ${root_path}/nohup.out &
 
 exit 0
