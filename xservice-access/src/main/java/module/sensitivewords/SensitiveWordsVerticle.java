f79cadce4aba771513ba4f2eb248fd947ce7116b
wanglonghu <wanglonghu@01zhuanche.com>
2017-10-17 10:42:10 +0800

    im

--- xservice-access/src/main/java/module/sensitivewords/SensitiveWordsVerticle.java
+++ xservice-access/src/main/java/module/sensitivewords/SensitiveWordsVerticle.java
@@ -32,7 +32,7 @@ public class SensitiveWordsVerticle extends AbstractVerticle {
 		trie = Trie.builder().addKeyword("日").addKeyword("我曹").build();
 
 		eb = vertx.eventBus();
-		eb.<JsonObject>consumer(QuickPhraseVerticle.class.getName(), res -> {
+		eb.<JsonObject>consumer(SensitiveWordsVerticle.class.getName(), res -> {
 			MultiMap headers = res.headers();
 			JsonObject body = res.body();
 			if (headers != null) {
