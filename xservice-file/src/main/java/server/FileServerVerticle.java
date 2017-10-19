0a27d2d21230df454c4835cc2ef37d35b82ec7a5
wanglonghu <wanglonghu@01zhuanche.com>
2017-10-19 17:38:13 +0800

    im

--- xservice-file/src/main/java/server/FileServerVerticle.java
+++ xservice-file/src/main/java/server/FileServerVerticle.java
@@ -130,13 +130,13 @@ public class FileServerVerticle extends AbstractVerticle {
 							}
 
 							// amr->mp3 transcoding
-							if (msgBody.getMsgType() != null && 2 == msgBody.getMsgType().intValue()) {
-								DeliveryOptions tsOption = new DeliveryOptions();
-								tsOption.setSendTimeout(3000);
-								tsOption.addHeader("action", TranscodingVerticle.method.amrToMp3);
-								logger.info("send msg to TranscodingVerticle, file={}", uploadPath + uuid);
-								eb.send(TranscodingVerticle.class.getName() + innerIP, date + "/" + uuid, tsOption);
-							}
+//							if (msgBody.getMsgType() != null && 2 == msgBody.getMsgType().intValue()) {
+//								DeliveryOptions tsOption = new DeliveryOptions();
+//								tsOption.setSendTimeout(3000);
+//								tsOption.addHeader("action", TranscodingVerticle.method.amrToMp3);
+//								logger.info("send msg to TranscodingVerticle, file={}", uploadPath + uuid);
+//								eb.send(TranscodingVerticle.class.getName() + innerIP, date + "/" + uuid, tsOption);
+//							}
 
 							hashFuture.setHandler(res -> {
 								logger.info("msgRequest, hashFuture={}", res.result().body().encode());
