e9bf79f19dadbe5238984b65512464c7e63998b0
wanglonghu <wanglonghu@01zhuanche.com>
2017-10-27 14:44:53 +0800

    im imsession config

--- xservice-im/src/main/java/test/Tester.java
+++ xservice-im/src/main/java/test/Tester.java
@@ -3,11 +3,7 @@ package test;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import module.c2c.C2CVerticle;
-import module.persistence.MongoVerticle;
-import module.quickphrase.QuickPhraseVerticle;
-import server.IMServerVerticle;
-import server.RestIMVerticle;
+import module.session.IMSessionVerticle;
 
 public class Tester {
 
@@ -23,7 +19,7 @@ public class Tester {
 		/**
 		 * Instance should be 1 because of ehcache.
 		 */
-		// vertx.deployVerticle(SessionVerticle.class.getName());
+		vertx.deployVerticle(IMSessionVerticle.class.getName());
 
 		/**
 		 * Instance should be 1 because of ehcache.
@@ -59,7 +55,7 @@ public class Tester {
 	}
 
 	private static JsonObject config() {
-		String prop = "{\"mongo\":{\"hosts\":[{\"host\":\"10.20.10.100\",\"port\":27017},{\"host\":\"10.20.10.101\",\"port\":27017},{\"host\":\"10.20.10.102\",\"port\":27017}],\"serverSelectionTimeoutMS\":30000,\"maxPoolSize\":50,\"minPoolSize\":25,\"maxIdleTimeMS\":300000,\"maxLifeTimeMS\":3600000,\"waitQueueMultiple\":10,\"waitQueueTimeoutMS\":10000,\"maintenanceFrequencyMS\":2000,\"maintenanceInitialDelayMS\":500,\"username\":\"im-mc\",\"password\":\"im-mc\",\"authSource\":\"im-mc\",\"db_name\":\"im-mc\",\"connectTimeoutMS\":3000,\"socketTimeoutMS\":3000,\"sendBufferSize\":8192,\"receiveBufferSize\":8192,\"keepAlive\":true,\"heartbeat.socket\":{\"connectTimeoutMS\":3000,\"socketTimeoutMS\":3000,\"sendBufferSize\":8192,\"receiveBufferSize\":8192,\"keepAlive\":true},\"heartbeatFrequencyMS\":1000,\"minHeartbeatFrequencyMS\":500},\"mysql\":{\"mc-im\":{\"host\":\"192.168.0.18\",\"port\":3306,\"maxPoolSize\":100,\"username\":\"sqyc_test\",\"password\":\"sqyc_test@01zhuanche.com\",\"database\":\"mc_im\",\"charset\":\"UTF-8\",\"queryTimeout\":3000}},\"im\":[{\"innerIP\":\"10.10.10.193\",\"node\":\"111.206.162.233:4321\"}],\"file\":{\"upload.file.path.prefix\":\"/u01/projectCAR/xservice/xservice-file/\",\"download.file.server.prefix\":\":9090/mc-file/im/download.json?file=\"},\"service.host\":\"127.0.0.1\"}";
+		String prop = "{\"mongo\":{\"hosts\":[{\"host\":\"10.20.10.100\",\"port\":27017},{\"host\":\"10.20.10.101\",\"port\":27017},{\"host\":\"10.20.10.102\",\"port\":27017}],\"serverSelectionTimeoutMS\":30000,\"maxPoolSize\":50,\"minPoolSize\":25,\"maxIdleTimeMS\":300000,\"maxLifeTimeMS\":3600000,\"waitQueueMultiple\":10,\"waitQueueTimeoutMS\":10000,\"maintenanceFrequencyMS\":2000,\"maintenanceInitialDelayMS\":500,\"username\":\"im-mc\",\"password\":\"im-mc\",\"authSource\":\"im-mc\",\"db_name\":\"im-mc\",\"connectTimeoutMS\":3000,\"socketTimeoutMS\":3000,\"sendBufferSize\":8192,\"receiveBufferSize\":8192,\"keepAlive\":true,\"heartbeat.socket\":{\"connectTimeoutMS\":3000,\"socketTimeoutMS\":3000,\"sendBufferSize\":8192,\"receiveBufferSize\":8192,\"keepAlive\":true},\"heartbeatFrequencyMS\":1000,\"minHeartbeatFrequencyMS\":500},\"mysql\":{\"mc-im\":{\"host\":\"192.168.0.18\",\"port\":3306,\"maxPoolSize\":100,\"username\":\"sqyc_test\",\"password\":\"sqyc_test@01zhuanche.com\",\"database\":\"mc_im\",\"charset\":\"UTF-8\",\"queryTimeout\":3000}},\"im\":[{\"innerIP\":\"10.10.10.193\",\"node\":\"111.206.162.233:4321\"}],\"file\":{\"upload.file.path.prefix\":\"/u01/projectCAR/xservice/xservice-file/\",\"download.file.server.prefix\":\":9090/mc-file/im/download.json?file=\"},\"service.host\":\"127.0.0.1\",\"im.memory.size\": 50}";
 
 		JsonObject jsonObject = new JsonObject(prop);
 		return jsonObject;
