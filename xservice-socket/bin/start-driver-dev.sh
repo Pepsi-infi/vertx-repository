7c3b5f95bede4b913382375dbd62ba383857c9db
wanglonghu <wanglonghu@01zhuanche.com>
2017-11-06 14:09:23 +0800

    socket driver deve=

--- xservice-socket/bin/start-driver-dev.sh
+++ xservice-socket/bin/start-driver-dev.sh
@@ -27,7 +27,7 @@ nohup /usr/local/jdk1.8/bin/java \
 -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
 -Dlog4j.configurationFile=log4j2.xml \
 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7091 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
--Dconfig=dev \
+-Dconfig=driver-dev \
 -Dvertx.zookeeper.config=zookeeper-dev.json \
 -jar ${root_path}/xservice-socket-fat.jar >> ${root_path}/nohup.out &
 
