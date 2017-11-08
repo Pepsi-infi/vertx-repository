5577da0b6b42a0ff0fe323b4527095c342ef80e7
sdmjhca <sdmjhca@foxmail.com>
2017-11-08 09:57:31 +0800

    add branch 包含 常用短语

--- xservice-im/src/main/java/test/Tester.java
+++ xservice-im/src/main/java/test/Tester.java
@@ -38,6 +38,7 @@ public class Tester {
 
 		vertx.deployVerticle(RestIMVerticle.class.getName(), new DeploymentOptions().setConfig(config()));
 		vertx.deployVerticle(IMConsistentHashingVerticle.class.getName(), new DeploymentOptions().setConfig(config()));
+
 		
 		// vertx.deployVerticle(QuickPhraseVerticle.class.getName(), new
 		// DeploymentOptions().setConfig(config()));
