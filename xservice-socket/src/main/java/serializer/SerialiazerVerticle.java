84f53ff1656d28481bd16cdef8b5f9e5605a9d4c
wanglonghu <wanglonghu@01zhuanche.com>
2017-11-20 18:44:51 +0800

    socket 反序列化 worker

---
+++ xservice-socket/src/main/java/serializer/SerialiazerVerticle.java
@@ -0,0 +1,53 @@
+package serializer;
+
+import java.util.Map;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class SerialiazerVerticle extends AbstractVerticle {
+
+	private static final Logger logger = LoggerFactory.getLogger(SerialiazerVerticle.class);
+
+	private EventBus eb;
+
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+		super.start();
+
+		eb = vertx.eventBus();
+		eb.<String>consumer(SerialiazerVerticle.class.getName(), res -> {
+			MultiMap headers = res.headers();
+			String data = res.body();
+			if (headers != null) {
+				String action = headers.get("action");
+				switch (action) {
+				case "unserialize":
+					res.reply(unserialize(data));
+					break;
+				default:
+					res.reply(1);// Fail!
+					break;
+				}
+			}
+
+		});
+	}
+
+	private JsonArray unserialize(String data) {
+		JsonArray msgBody = null;
+		try {
+			Map<String, Object> map = (Map<String, Object>) SocketByteUtils.byteToObject(data.getBytes());
+			msgBody = (JsonArray) map.get("params");
+		} catch (Exception e) {
+			logger.error("unserialize, data={}e={}", data, e.getMessage());
+		}
+
+		return msgBody;
+	}
+}
--- xservice-socket/src/main/java/server/UdpServerVerticle.java
+++ xservice-socket/src/main/java/server/UdpServerVerticle.java
@@ -1,6 +1,6 @@
 package server;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -13,10 +13,12 @@ import io.vertx.core.datagram.DatagramSocketOptions;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.rxjava.core.Future;
+import serializer.SerialiazerVerticle;
 import serializer.SocketByteUtils;
 import utils.IPUtil;
 
@@ -43,68 +45,89 @@ public class UdpServerVerticle extends AbstractVerticle {
 					Map<String, Object> map = null;
 
 					try {
+						DeliveryOptions option = new DeliveryOptions();
+						option.setSendTimeout(3000);
+						option.addHeader("action", "unserialize");
+
+						Future<Message<JsonArray>> unsFuture = Future.future();
+
+						eb.<JsonArray>send(SerialiazerVerticle.class.getName(), packet.data().toString(), option,
+								unsFuture.completer());
+						unsFuture.setHandler(r -> {
+							if (r.succeeded()) {
+								JsonArray msgList = r.result().body();
+								if (msgList != null) {
+									List<Object> msgBody = msgList.getList();
+									logger.info("UDP Map " + msgBody.toString());
+
+									try {
+										// ArrayList<Object> msgBody = (ArrayList<Object>) res.get("params");
+										final String userId = String.valueOf(msgBody.get(0));// userId
+										int cmd = NumberUtils.toInt(String.valueOf(msgBody.get(1)));
+
+										//
+										DeliveryOptions scOption = new DeliveryOptions();
+										scOption.setSendTimeout(3000);
+										scOption.addHeader("action", "getInnerNode");
+
+										Future<Message<JsonObject>> chFuture = Future.future();
+
+										JsonObject message = new JsonObject();
+										message.put("userId", userId);
+										if (StringUtils.isNotEmpty(userId)) {
+											eb.<JsonObject>send(
+													SocketConsistentHashingVerticle.class.getName() + innerIP, message,
+													scOption, chFuture.completer());
+										} else {
+
+										}
+
+										Future<Message<JsonObject>> ssFuture = Future.future();
+
+										chFuture.setHandler(chRes -> {
+											if (chRes.succeeded()) {
+												JsonObject jsonRes = chRes.result().body();
+												String hostIP = jsonRes.getString("host");
+
+												DeliveryOptions msOption = new DeliveryOptions();
+												msOption.setSendTimeout(3000);
+												msOption.addHeader("action", "sendMsg");
+
+												logger.info("UDP userId={}innerIP={}", userId, hostIP);
+
+												JsonObject data = JsonObject.mapFrom(msgBody.get(3));
+
+												JsonObject msg2Send = new JsonObject();
+												msg2Send.put("cmd", cmd);
+												msg2Send.put("data", data);
+
+												JsonObject param = new JsonObject();
+												param.put("userId", userId);
+												param.put("msg", msg2Send);
+
+												eb.<JsonObject>send(MessageSendVerticle.class.getName() + hostIP, param,
+														msOption, ssFuture.completer());
+
+											} else {
+
+											}
+										});
+									} catch (Exception e2) {
+										logger.error("Get userId error ", e2);
+									}
+
+								}
+							} else {
+
+							}
+						});
+
 						map = (Map<String, Object>) SocketByteUtils.byteToObject(packet.data().getBytes());
 					} catch (Exception e) {
 						logger.error("UDP unserialize packet={}e={}", packet.data(), e.getCause());
 					}
 
 					if (map != null) {
-						logger.info("UDP Map " + map.toString());
-
-						try {
-							ArrayList<Object> msgBody = (ArrayList<Object>) map.get("params");
-							final String userId = String.valueOf(msgBody.get(0));// userId
-							int cmd = NumberUtils.toInt(String.valueOf(msgBody.get(1)));
-
-							//
-							DeliveryOptions option = new DeliveryOptions();
-							option.setSendTimeout(3000);
-							option.addHeader("action", "getInnerNode");
-
-							Future<Message<JsonObject>> chFuture = Future.future();
-
-							JsonObject message = new JsonObject();
-							message.put("userId", userId);
-							if (StringUtils.isNotEmpty(userId)) {
-								eb.<JsonObject>send(SocketConsistentHashingVerticle.class.getName() + innerIP, message,
-										option, chFuture.completer());
-							} else {
-
-							}
-
-							Future<Message<JsonObject>> ssFuture = Future.future();
-
-							chFuture.setHandler(res -> {
-								if (res.succeeded()) {
-									JsonObject jsonRes = res.result().body();
-									String hostIP = jsonRes.getString("host");
-
-									DeliveryOptions msOption = new DeliveryOptions();
-									msOption.setSendTimeout(3000);
-									msOption.addHeader("action", "sendMsg");
-
-									logger.info("UDP userId={}innerIP={}", userId, hostIP);
-
-									JsonObject data = JsonObject.mapFrom(msgBody.get(3));
-
-									JsonObject msg2Send = new JsonObject();
-									msg2Send.put("cmd", cmd);
-									msg2Send.put("data", data);
-
-									JsonObject param = new JsonObject();
-									param.put("userId", userId);
-									param.put("msg", msg2Send);
-
-									eb.<JsonObject>send(MessageSendVerticle.class.getName() + hostIP, param, msOption,
-											ssFuture.completer());
-
-								} else {
-
-								}
-							});
-						} catch (Exception e2) {
-							logger.error("Get userId error ", e2);
-						}
 					} else {
 						logger.info("Map is null.");
 					}
