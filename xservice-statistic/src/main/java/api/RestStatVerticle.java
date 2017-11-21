dc7bb00b05b0f4a793f4628439e7b0dab345e4f1
ylf <yanglongfei@01zhuanche.com>
2017-11-21 13:02:09 +0800

    正则校验bug修复

--- xservice-statistic/src/main/java/api/RestStatVerticle.java
+++ xservice-statistic/src/main/java/api/RestStatVerticle.java
@@ -198,7 +198,7 @@ public class RestStatVerticle extends RestAPIVerticle {
 	}
 
 	private boolean matchVersion(String appVersion) {
-		String regex = "^([1-9][0-9]+)(\\.[1-9][0-9]+)(\\.[1-9][0-9]+)";
+		String regex = "^([1-9][0-9]+)(\\.[1-9]?[0-9]+)(\\.[1-9]?[0-9]+)";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(appVersion);
 		return matcher.matches();
