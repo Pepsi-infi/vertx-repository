746f89001624f7dcefc261e5608cb08492016796
wanglonghu <wanglonghu@01zhuanche.com>
2017-11-23 18:44:19 +0800

    socket passenger

---
+++ xservice-proxy/bin/start-passenger-dev.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -x
+root_path=$(cd "$(dirname "${0}")"; pwd)
+
+pid=$(ps -ef | grep xservice-proxy | grep java | awk '{print $2}')
+if [ ! -z "$pid" ]
+then 
+  kill -9 $pid
+fi
+
+sleep 5s
+
+nohup /usr/local/jdk1.8/bin/java \
+-server \
+-XX:+PrintGCApplicationStoppedTime \
+-XX:+PrintGCTimeStamps \
+-XX:+PrintGCDetails \
+-Xms1g -Xmx1g -Xmn768m -Xss256K -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m \
+-XX:AutoBoxCacheMax=20000 -XX:+AlwaysPreTouch \
+-XX:+UseParallelOldGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly \
+-XX:MaxTenuringThreshold=2 -XX:+ExplicitGCInvokesConcurrent \
+-XX:-UseCounterDecay \
+-Djava.net.preferIPv4Stack=true \
+-Xloggc:${root_path}/gc.log \
+-Dlog.path=${root_path}/log \
+-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
+-Dlog4j.configurationFile=log4j2.xml \
+-Dconfig=passenger-dev \
+-Dvertx.zookeeper.config=zookeeper-dev.json \
+-jar ${root_path}/xservice-proxy-fat.jar >> ${root_path}/nohup.out &
+
+sleep 2s
+
+exit 0
